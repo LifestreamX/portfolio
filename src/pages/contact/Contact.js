c9b360f76e8ab6478a6257559bd5bdb4452f5cc7
haloandfable <tylerallen@live.com>
2022-10-06 19:11:51 -0400

    styled contact page mobile first

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -99,91 +99,82 @@ const Contact = () => {
 
   return (
     <motion.div
-      className='container'
+      className='contact-page'
       initial={{ width: 0 }}
       animate={{ width: '100%' }}
       exit={{ x: window.innerWidth, transition: { duration: 0.5 } }}
     >
-      <main className='container'>
-        {/* Left side animation logo */}
-        <animated.div
-          className='animation-wrapper'
-          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-          onMouseLeave={() => set({ xys: [0, 0, 1] })}
-          style={{
-            transform: props.xys.to(trans),
-          }}
-        >
-          <FontAwesomeIcon icon='envelope' className=' contact-page-logo ' />
-        </animated.div>
+      {/* TITLE */}
+      <h1 className='input-1'>CONTACT ME</h1>
+      {/* LOGO */}
+      <animated.div
+        className='animation-wrapper'
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{
+          transform: props.xys.to(trans),
+        }}
+      >
+        <FontAwesomeIcon icon='envelope' className=' contact-page-logo ' />
+      </animated.div>
 
-        {/* Right side form */}
-        <div className='inner'>
-          <div className='panel panel-left'>
-            <div className='panel-content'>
-              <div className='image-background'></div>
-            </div>
+      {/* FORM  */}
+      {/* Form */}
+      <form className='form' ref={form} onSubmit={sendEmail}>
+        {sent === true && (
+          <Alert className='alert' severity='success'>
+            Email has been sent.
+          </Alert>
+        )}
+
+        <div className='form-wrapper'>
+          {/* Name input */}
+          <div className='group input-2'>
+            <input type='text' required name='name' />
+            <span className='highlight'></span>
+            <label className='labels'>Name</label>
           </div>
-          <div className='panel panel-right'>
-            <div className='panel-content'>
-              {/* Form */}
-              <form className='form' ref={form} onSubmit={sendEmail}>
-                <h1 className='input-1'>CONTACT ME</h1>
-
-                {sent === true && (
-                  <Alert className='alert' severity='success'>
-                    Email has been sent.
-                  </Alert>
-                )}
-
-                {/* Name input */}
-                <div className='group input-2'>
-                  <input type='text' required name='name' />
-                  <span className='highlight'></span>
-                  <label className='labels'>Name</label>
-                </div>
-                {/* Email input */}
-                <div className='group input-3'>
-                  <input type='email' required name='email' />
-                  <span className='highlight'></span>
-                  <label for='email' className='labels'>Email</label>
-                </div>
-                {/* Subject input */}
-                <div className='group input-4'>
-                  <input type='text' required name='subject' />
-                  <span className='highlight'></span>
-                  <label className='labels'>Subject</label>
-                </div>
-                {/* Message input */}
-                <div className='group message-wrapper input-5'>
-                  <textarea required className='message-box' name='message' />
-                  <label>Message</label>
-                </div>
-                {/* Send button  */}
-                <button type='submit'>
-                  <div class='svg-wrapper-1'>
-                    <div class='svg-wrapper'>
-                      <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        viewBox='0 0 24 24'
-                        width='24'
-                        height='24'
-                      >
-                        <path fill='none' d='M0 0h24v24H0z'></path>
-                        <path
-                          fill='currentColor'
-                          d='M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z'
-                        ></path>
-                      </svg>
-                    </div>
-                  </div>
-                  <span>Send</span>
-                </button>
-              </form>
-            </div>
+          {/* Email input */}
+          <div className='group input-3'>
+            <input type='email' required name='email' />
+            <span className='highlight'></span>
+            <label for='email' className='labels'>
+              Email
+            </label>
           </div>
+          {/* Subject input */}
+          <div className='group input-4'>
+            <input type='text' required name='subject' />
+            <span className='highlight'></span>
+            <label className='labels'>Subject</label>
+          </div>
+          {/* Message input */}
+          <div className='group message-wrapper input-5'>
+            <textarea required className='message-box' name='message' />
+            <label>Message</label>
+          </div>
+          {/* Send button  */}
+          <button type='submit'>
+            <div class='svg-wrapper-1'>
+              <div class='svg-wrapper'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 24 24'
+                  width='24'
+                  height='24'
+                >
+                  <path fill='none' d='M0 0h24v24H0z'></path>
+                  <path
+                    fill='currentColor'
+                    d='M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z'
+                  ></path>
+                </svg>
+              </div>
+              <span className='send-button'>Send</span>
+            </div>
+          </button>
         </div>
-      </main>
+      </form>
     </motion.div>
   );
 };
