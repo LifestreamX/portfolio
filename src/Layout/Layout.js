672af9db61f72f48718412b7db4a0821197de804
haloandfable <tylerallen@live.com>
2022-11-18 16:51:37 -0500

    updated resize component for navbars

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -9,28 +9,10 @@ const Layout = () => {
 
   const [click, setClick] = useState(false);
 
-  //choose the screen size
-  const handleResize = () => {
-    if (window.innerWidth < 1025) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-  };
-
-  // create an event listener
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-  }, []);
-
-
-
   return (
     <div>
-      {window.innerWidth < 1025 && (
-        <MobileNav click={click} setClick={setClick} />
-      )}
-      {window.innerWidth > 1025 && <Sidebar />}
+      <MobileNav click={click} setClick={setClick} />
+      <Sidebar />
 
       <div className='page' id={click && 'hide-background'}>
         <Outlet />
